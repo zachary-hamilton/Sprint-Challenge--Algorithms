b3c3f680b54665c5e6100b13405d8c209f58124e
Zach Hamilton <zlhamilton90@gmail.com>
2020-10-12 00:22:47 -0400

    finished robot sort, at 12:22

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -105,34 +105,23 @@ class SortingRobot:
         self.set_light_on()
         while self.light_is_on():
             self.swap_item()
-            self.move_right()
             while self.can_move_right():
+                self.move_right()
                 if self.compare_item() == 1:
                     self.swap_item()
-                self.move_right()
             while self.compare_item() != None:
                 self.move_left()
+            self.swap_item()
             if self.can_move_right() == False:
-                self.swap_item()
                 self.set_light_off()
-            else:
-                self.swap_item()
-
-
-        
-            
-
-            
-            
-            
-
-
+            self.move_right()
+          
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-    l = [2, 1, 3, 4, 2]
+    l = [3, 2, 4, 2, 1]
     robot = SortingRobot(l)
 
     robot.sort()
