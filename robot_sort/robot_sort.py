a0ae1ad59657fdc7381b87912fc2371bc3e0d6a2
Zach Hamilton <zlhamilton90@gmail.com>
2020-10-11 23:56:56 -0400

    still working on robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,35 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # need a selection sort
+        # loop through whole list and swap if item is lower
+        # return to where the none is on the list
+        # swap the lowest item with the none item
+        # move to the right one
+        # repeat this until cant move right anymore
+        self.set_light_on()
+        while self.light_is_on():
+            self.swap_item()
+            self.move_right()
+            while self.can_move_right():
+                if self.compare_item() == 1:
+                    self.swap_item()
+                self.move_right()
+            while self.compare_item() != None:
+                self.move_left()
+            if self.can_move_right() == False:
+                self.swap_item()
+                self.set_light_off()
+            else:
+                self.swap_item()
+
+
+        
+            
+
+            
+            
+            
 
 
 if __name__ == "__main__":
@@ -105,7 +132,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    l = [2, 1, 3, 4, 2]
     robot = SortingRobot(l)
 
     robot.sort()
